d3874ed47cce029fa5aee83d396a40cd2aa115ca
anmportugal <anm@nanoportugal.com>
2026-03-02 12:18:48 +0530

    Update Deadlines.jsx

--- src/components/Deadlines.jsx
+++ src/components/Deadlines.jsx
@@ -8,12 +8,12 @@ export default function DeadlinesPage() {
     },
     {
       title: "Early Bird Registration",
-      date: "28 February 2026",
+      date: "31 March 2026",
       icon: "M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
     },
     {
       title: "Normal Registration",
-      date: "31 March 2026",
+      date: "30 April 2026",
       icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
     },
     {
