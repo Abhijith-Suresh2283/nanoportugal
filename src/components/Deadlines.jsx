b5a8eddedcefe1ef75d8f10a2579d06d2cac1475
Abhijith Suresh <128500350+Abhijith-Suresh2283@users.noreply.github.com>
2026-04-26 21:26:45 +0530

    Removed early bird registration

--- src/components/Deadlines.jsx
+++ src/components/Deadlines.jsx
@@ -7,11 +7,6 @@ export default function DeadlinesPage() {
       icon: "M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
     },
     {
-      title: "Early Bird Registration",
-      date: "31 March 2026",
-      icon: "M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-    },
-    {
       title: "Normal Registration",
       date: "30 April 2026",
       icon: "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
