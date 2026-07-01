b0ab6a22eea8c0573fe6dd5631184838802010f7
anmportugal <anm@nanoportugal.com>
2026-07-01 10:32:33 +0100

    Update Registration.jsx

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -69,7 +69,7 @@ export default function RegistrationPage() {
                 <tr className="border-b border-gray-300 bg-gray-50">
                   <th className="text-left p-3 font-normal border-r border-gray-300 text-sm"></th>
                   <th className="text-left p-3 font-normal border-r border-gray-300 text-sm">Until 31 May 2026</th>
-                  <th className="text-left p-3 font-normal text-sm">Until 30 June 2026</th>
+                  <th className="text-left p-3 font-normal text-sm">Until 15 July 2026</th>
                 </tr>
               </thead>
               <tbody>
