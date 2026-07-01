ca0c8d809bf1a5ef104165516e325597c928d947
anmportugal <anm@nanoportugal.com>
2026-07-01 10:26:47 +0100

    Update Deadlines.jsx

--- src/components/Deadlines.jsx
+++ src/components/Deadlines.jsx
@@ -3,7 +3,7 @@ export default function DeadlinesPage() {
   const deadlines = [
     {
       title: "Abstract Submission",
-      date: "20 June 2026",
+      date: "10 July 2026" (virtual),
       icon: "M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
     },
     {
@@ -13,7 +13,7 @@ export default function DeadlinesPage() {
     },
     {
       title: "Late Registration",
-      date: "30 June 2026",
+      date: "15 July 2026",
       icon: "M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
     },
     {
